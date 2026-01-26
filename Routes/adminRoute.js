61db7307022674a2438ecf7af6ab8adef34a7dc2
abhinendrasingh3 <abhinendrayadav3@gmail.com>
2026-01-26 10:50:20 +0530

    adminRoute

--- Routes/adminRoute.js
+++ Routes/adminRoute.js
@@ -1,23 +1,44 @@
-const express=require('express');
-const adminRoute=express.Router;
-const jwtAuthMiddleWare=require('./../jwtAuthMiddleWare');
+const express = require('express');
+const adminRoute = express.Router;
+const jwtAuthMiddleWare = require('./../jwtAuthMiddleWare');
 const adminAuth = require('../adminAuth');
 const users = require('./../Models/users');
+const bcrypt = require('bcrypt');
+const adminAuth = require('./../adminAuth')
 
-adminRoute.post('/login',async(req,res)=>{
+adminRoute.post('/login', async (req, res) => {
+try{
+    const { email, password } = req.body;
 
-    const {email,password}=req.body;
+    //checks in db
+    const checkUser = users.findOne({ email: email })
+    if (!checkUser) {
+        return res.status(401).json({ message: "User not valid" })
+    }
 
-//checks in db
-const checkUser=users.findOne({email:email})
-if(!checkUser){
-    return res.status(401).json({message:"user not valid"})
+    //password is correct or not 
+    const checkPass = bcrypt.compare(password, checkUser.password)
+    if (!checkPass) {
+        res.status(401).json({ message: "Invalid User Password" })
+    }
+
+    //checks role
+    if(checkUser.role!=='admin'){
+        return res.status(403).json({message:"only admins are allowed to login"})
+    }
+
+    //generate jwt tokens
+        //generate payload
+        //generate token=jwtsignin
+
+    
+
+
+
+
 }
-
-//checks role
-//generate jwt tokens
+catch(err){
 
 
-
-
+}
 })
